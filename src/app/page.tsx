5d78042f5b31f969433abf6987a85b57fdd2590e
Roy Dey <roydey10@gmail.com>
2024-11-18 20:51:14 -0800

    fix scroll behavior

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ import TechnologiesSection from "@/components/portfolio/TechnologiesSection";
 
 export default function Home() {
   return (
-    <div className="grid grid-flow-row grid-rows-[auto,1fr] justify-center px-5 sm:px-20 2xl:justify-normal 2xl:px-0 2xl:grid-flow-col 2xl:grid-rows-1 mx-auto 2xl:gap-40 bg-charcoal-midnight h-screen overflow-auto">
-      <div className="pt-32 mb-24 2xl:pl-10">
-        <div className="w-fit 2xl:ml-auto">
+    <div className="grid grid-flow-row grid-rows-[auto,1fr] justify-center px-6 sm:px-20 2xl:justify-normal 2xl:px-0 2xl:grid-flow-col 2xl:grid-rows-1 mx-auto 2xl:gap-40 bg-charcoal-midnight h-screen overflow-y-scroll relative">
+      <div className="pt-32 mb-24 2xl:pl-10 2xl:sticky 2xl:top-0">
+        <div className="w-fit 2xl:ml-auto relative">
           <ProfileSection />
         </div>
       </div>
-      <div className="2xl:pt-32 2xl:pr-10 2xl:overflow-y-scroll pb-32 2xl:pl-10">
+      <div className="2xl:pt-32 2xl:pr-10 pb-32 2xl:pl-10 h-fit">
         <div className="flex flex-col max-w-[768px] gap-[100px]">
           <TechnologiesSection />
           <ExperienceSection />
